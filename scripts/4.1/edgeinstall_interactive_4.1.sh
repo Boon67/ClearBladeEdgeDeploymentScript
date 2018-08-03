ae4c10e9885906c4a2d44f6167c546c66a8be89a
Terrance Boon <Boon67@msn.com>
2018-08-03 17:48:40 -0600

    Update edgeinstall_interactive_4.1.sh

--- scripts/4.1/edgeinstall_interactive_4.1.sh
+++ scripts/4.1/edgeinstall_interactive_4.1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 RELEASE="4.1.2" #Edge Version to Install this does get updated often
-echo "---------ClearBlade Interactive Edge Installer v0.9.2---------"
-echo "Edge Release: $RELEASE"
+echo "---------ClearBlade Interactive Edge Installer v0.9.1---------"
+echo "Edge Release: $RELEASE" 
 
 if [ "$EUID" -ne 0 ]
-  then
+  then 
     echo "---------Permissions Error---------"
     echo "STOPPING: Please run as root or sudo"
     echo "-----------------------------------"
@@ -14,29 +14,26 @@ fi
 #---------Ensure your architecture is correct----------
 MACHINE_ARCHITECTURE="$(uname -m)"
 MACHINE_OS="$(uname)"
-echo "Machine Architecture: $MACHINE_ARCHITECTURE"
+echo "Machine Architecture: $MACHINE_ARCHITECTURE" 
 if [ "$MACHINE_ARCHITECTURE" == "armv6l" ] ; then
-  ARCHITECTURE="edge-linux-armv6.tar.gz"
+  ARCHITECTURE="edge-linux-arm.tar.gz"
 elif [ "$MACHINE_ARCHITECTURE" == "armv7l" ] ; then
-  ARCHITECTURE="edge-linux-armv7.tar.gz"
+  ARCHITECTURE="edge-linux-arm.tar.gz"
 elif [ "$MACHINE_ARCHITECTURE" == "armv8" ] ; then
   ARCHITECTURE="edge-linux-arm64.tar.gz"
 elif [ "$MACHINE_ARCHITECTURE" == "i686" ] ||  [ "$MACHINE_TYPE" == "i386" ] ; then
   ARCHITECTURE="edge-linux-386.tar.gz"
 elif [ "$MACHINE_ARCHITECTURE" == "x86_64" ] && [ "$MACHINE_OS" == "Darwin" ] ; then
-  ARCHITECTURE="edge-darwin-amd64.tar.gz"
+  ARCHITECTURE="edge-darwin-amd64.tar.gz" 
 elif [ "$MACHINE_ARCHITECTURE" == "x86_64" ] && [ "$MACHINE_OS" == "Linux" ] ; then
   ARCHITECTURE="edge-linux-amd64.tar.gz"
-else
+else 
   echo "---------Unknown Architecture Error---------"
     echo "STOPPING: Validate Architecture of OS"
     echo "-----------------------------------"
   exit
 fi
 
-
-#---------Logging Info---------
-LOGLEVEL="debug"
 read -p "Enter Edge Token: " EDGECOOKIE
 read -p "Enter Edge Name: " EDGEID
 read -p "Enter System Key: " PARENTSYSTEM
@@ -50,18 +47,20 @@ read -p "Enter Platform FQDN: " PLATFORMFQDN
 
 #----------FILESYSTEM SETTINGS FOR EDGE
 BINPATH=/usr/bin
-DEPLOYMENTFOLDER="clearblade"
-CBBINPATH=$BINPATH/$DEPLOYMENTFOLDER
-EDGEDBPATH=$BINPATH/$DEPLOYMENTFOLDER
+VARPATH=/var/lib
+CBBINPATH=$BINPATH/clearblade
+EDGEDBPATH=$VARPATH/clearblade
+EDGEUSERSDBPATH=$VARPATH/clearblade
 EDGEUSERDBNAME=edgeusers.db
 EDGEDBNAME=edge.db
 DISABLEPPROF=true
 
 #---------Edge Version---------
-EDGEBIN="$BINPATH/$DEPLOYMENTFOLDER/edge"
-DATASTORE="-db=sqlite -sqlite-path=$EDGEDBPATH/$EDGEDBNAME -sqlite-path-users=$EDGEDBPATH/$EDGEUSERDBNAME" # or "-local"
+EDGEBIN="$BINPATH/clearblade/edge"
+DATASTORE="-db=sqlite -sqlite-path=$EDGEDBPATH/$EDGEDBNAME -sqlite-path-users=$EDGEUSERSDBPATH/$EDGEUSERDBNAME" # or "-local"
 
-
+#---------Logging Info---------
+LOGLEVEL="info"
 #---------Systemd Configuration---------
 SYSTEMDPATH="/lib/systemd/system"
 SYSTEMDSERVICENAME="clearblade.service"
@@ -70,9 +69,11 @@ SERVICENAME="ClearBlade Edge Service"
 
 echo ---------------------1. Installing Prereqs---------------------
 #---------Pre Reqs-------------------
-apt-get update && time
-apt-get dist-upgrade -y
-apt-get install curl -y
+#apt-get update && time 
+#apt-get dist-upgrade -y
+if ! curl_loc="$(type -p "curl")" || [ -z "$curl_loc"]; then
+ apt-get install curl -y
+fi
 
 #---------Edge Configuration---------
 echo ---------------------2. Edge Config---------------------
@@ -96,22 +97,20 @@ echo "SERVICENAME: $SERVICENAME"
 echo ---------------------3. Cleaning old systemd services and binaries---------------------
 systemctl stop $SYSTEMDSERVICENAME
 systemctl disable $SYSTEMDSERVICENAME
-#pkill -x edge
-rm $SYSTEMDPATH/$SYSTEMDSERVICENAME
 
+systemctl stop $SYSTEMDSERVICENAME
+systemctl disable $SYSTEMDSERVICENAME
+rm $SYSTEMDPATH/$SYSTEMDSERVICENAME
 rm -rf $SYSTEMDSERVICENAME
-echo Deleting ClearBlade BIN Folder
-rm -rf /usr/bin/clearblade
+rm -rf /var/lib/clearblade
+rm "$EDGEBIN"
 systemctl daemon-reload
 
 echo ---------------------4. Creating File Structure---------------------
-mkdir $BINPATH #Just in case bin doesn't exist in /usr/bin
-echo $CBBINPATH
+mkdir $BINPATH #Just in case bin doesn't exist in /usr/local
 mkdir $CBBINPATH
-touch $EDGEDBPATH/$EDGEDBNAME
-chmod 664 $EDGEDBPATH/$EDGEDBNAME 
-touch $EDGEDBPATH/$EDGEUSERDBNAME
-chmod 664 $EDGEDBPATH/$EDGEUSERDBNAME
+mkdir $EDGEDBPATH
+mkdir $EDGEUSERSDBPATH
 
 echo ---------------------5. Downloading Edge---------------------
 echo "https://github.com/ClearBlade/Edge/releases/download/$RELEASE/$ARCHITECTURE"
@@ -120,7 +119,8 @@ curl -#SL -L "https://github.com/ClearBlade/Edge/releases/download/$RELEASE/$ARC
 echo ---------------------6. Installing Edge---------------------
 tar xzvf /tmp/$ARCHITECTURE
 ln -f "edge-$RELEASE" "$EDGEBIN"
-chmod 774 "$EDGEBIN"
+chmod +x "$EDGEBIN"
+
 rm /tmp/$ARCHITECTURE
 
 echo ---------------------7. Creating clearblade service---------------------
@@ -128,21 +128,16 @@ echo ---------------------7. Creating clearblade service---------------------
 cat >$SYSTEMDSERVICENAME <<EOF
 [Unit]
 Description=$SERVICENAME Version: $RELEASE
-After=network-online.target
-Wants=network-online.target
 
 [Service]
 Type=simple
 WorkingDirectory=$CBBINPATH
-ExecStart=$EDGEBIN -log-level=$LOGLEVEL -novi-ip=$PLATFORMFQDN -parent-system=$PARENTSYSTEM -edge-ip=localhost -edge-id=$EDGEID -edge-cookie=$EDGECOOKIE $DATASTORE -disable-pprof=$DISABLEPPROF
+ExecStart=$EDGEBIN -log-level=$LOGLEVEL -novi-ip=$PLATFORMFQDN -parent-system=$PARENTSYSTEM -edge-ip=localhost -edge-id=$EDGEID -edge-cookie=$EDGECOOKIE $DATASTORE -disable-pprof=$DISABLEPPROF 
 Restart=on-abort
 TimeoutSec=30
 RestartSec=30
 StartLimitInterval=350
 StartLimitBurst=10
-StandardOutput=syslog
-StandardError=syslog
-SyslogIdentifier=ClearBladeEdge
 
 [Install]
 WantedBy=multi-user.target
@@ -168,7 +163,6 @@ do
   printf "\b${sp:i++%${#sp}:1}"
 done
 
+systemctl status $SYSTEMDSERVICENAME
 
 echo "Run ----'systemctl status $SYSTEMDSERVICENAME'------for status"
-systemctl status $SYSTEMDSERVICENAME
-echo pgrep edge
